fe825acc5ebddaea3a5e5f2117695b5e6a59c942
JL1829 <joh@johdev.com>
2022-04-08 03:16:16 +0000

    Start developing preprocess module

---
+++ preprocess/simplepreprocessor.py
@@ -0,0 +1,9 @@
+class SimplePreprocessor:
+    """
+    A simple preprocessor to preprocess image
+    """
+    def __init__(self) -> None:
+        raise NotImplementedError
+    
+    def process(self) -> None:
+        raise NotImplementedError
